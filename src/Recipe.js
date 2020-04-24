f987178091a7f8e29a6158d3074b4fff596a6f89
m0nds <elegbede.raymond@gmail.com>
2020-04-24 02:24:17 +0100

    first commit

---
+++ src/Recipe.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './recipe.module.css';
+
+const Recipe = ({ title, image, ingredients}) => {
+    return(
+        <div className={style.recipe}>
+            <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredients =>(
+                    <li>{ingredients.text}</li>
+                ))}
+            </ol>
+            <img src={image} alt ="" />
+        </div>
+    );
+}
+export default Recipe;
